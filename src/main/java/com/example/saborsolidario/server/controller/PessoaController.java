0d4f1e07a8ed69d392e722d50bdf5cbe1dab3f42
CeschimLuigi <luigifaggeceschim@gmail.com>
2024-09-28 22:20:57 -0300

    Post mapping

--- src/main/java/com/example/saborsolidario/server/controller/PessoaController.java
+++ src/main/java/com/example/saborsolidario/server/controller/PessoaController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class PessoaController {
     @Autowired
     private PessoaService service;
 
-
+    @PostMapping
     public ResponseEntity<String>cadastrar(@Valid @RequestBody PessoaRequest request){
 
        return service.execute(request);
